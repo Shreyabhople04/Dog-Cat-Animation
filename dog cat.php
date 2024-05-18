7232fa503c14085f6fdd182b4371e12947dfdcf7
Shreyabhople04 <sarikabhople83@gmail.com>
2024-05-18 12:53:15 +0530

    Car Animation Upload Successfully

---
+++ dog cat.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Run Animation</title>
+</head>
+
+<div class="container">
+    <div class="mouse">
+        <img src="img/c.gif">
+    </div>
+
+    <div class="cat">
+        <img src="img/b.gif">
+    </div>
+
+    <div class="dog">
+        <img src="img/a.gif">
+    </div>
+</div>
+
+
+<body>
+</html>
